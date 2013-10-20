700c3b9b4e4fa9f8d0470ed5281aa4f91a913fc2
Kenneth Cachia <kcachia@gmail.com>
2013-10-20 19:45:29 -0400

    Move UI code to drag.js

--- examples/cross-origin.js
+++ examples/cross-origin.js
@@ -1,67 +1,7 @@
 
 /* global BackgroundCheck:false */
 
-var drag,
-    offset,
-    demo,
-    touch = document.ontouchstart !== undefined ? true : false;
-
-
-function updatePosition(e) {
-  var wrapper = {
-    x: demo.offsetLeft,
-    y: demo.offsetTop
-  };
-
-  if (touch) {
-    e.preventDefault();
-  }
-
-  drag.style.left = ((e.pageX - offset.x) - wrapper.x) + 'px';
-  drag.style.top = ((e.pageY - offset.y) - wrapper.y) + 'px';
-}
-
-
-function getUIElement(el, child) {
-  var name = 'border',
-      required;
-
-  if (el.className.indexOf(name) !== -1) {
-    required = child ? el.children[0] : el;
-  } else if (el.parentNode.className.indexOf(name) !== -1) {
-    required = child ? el : el.parentNode;
-  }
-
-  return required;
-}
-
-
 window.addEventListener('DOMContentLoaded', function () {
-  demo = document.querySelector('.demo');
-
-  // Click
-  window.addEventListener(touch ? 'touchstart' : 'mousedown', function (e) {
-    drag = null;
-    drag = getUIElement(e.target);
-
-    if (drag) {
-      offset = { 
-        x: e.offsetX || e.layerX,
-        y: e.offsetY || e.layerY
-      };
-
-      window.addEventListener(touch ? 'touchmove' : 'mousemove', updatePosition);
-    }
-  });
-
-  // Release
-  window.addEventListener(touch ? 'touchend' : 'mouseup', function () {
-    if (drag) {
-      window.removeEventListener(touch ? 'touchmove' : 'mousemove', updatePosition);
-      BackgroundCheck.refresh(getUIElement(drag, true));
-    }
-  });
-
   // Load image
   var img = document.querySelector('img');
   img.src = img.getAttribute('data-src');
