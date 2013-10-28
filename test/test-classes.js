83bf6801ea88e81f6d39eb9c44779e6fa498a2bf
Kenneth Cachia <kcachia@gmail.com>
2013-10-27 23:10:16 -0400

    Test light, dark and complex classes.

---
+++ test/test-classes.js
@@ -0,0 +1,19 @@
+
+test('Test light, dark and complex classes', function () {
+
+  var target1 = wrapper.querySelector('.test .target--one');
+  var target2 = wrapper.querySelector('.test .target--two');
+
+  wrapper.style.display = 'block';
+
+  BackgroundCheck.init({
+    targets: '.test .target',
+    images: '.test img'
+  })
+
+  equal(target1.className, 'target target--one background--dark', 'correct classes are added');
+  equal(target2.className, 'target target--two background--light background--complex', 'correct classes are added');
+
+  wrapper.style.display = 'none';
+
+});
