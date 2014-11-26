9f3481d7e96983a4d51155f56f0d041f3e40dd74
Daniel Korte <daniel@toky.com>
2014-11-26 11:29:45 -0600

    Remove window event listeners when plugin is destroyed

--- background-check.js
+++ background-check.js
@@ -70,12 +70,12 @@
         canvas.style.width = '100%';
         canvas.style.height = '100%';
 
-        window.addEventListener(resizeEvent, throttle.bind(null, function () {
+        window.addEventListener(resizeEvent + '.backgroundCheck', throttle.bind(null, function () {
           resizeCanvas();
           check();
         }));
 
-        window.addEventListener('scroll', throttle.bind(null, check));
+        window.addEventListener('scroll.backgroundCheck', throttle.bind(null, check));
 
         resizeCanvas();
         check();
@@ -88,6 +88,11 @@
    * Destructor
    */
   function destroy() {
+    if (supported) {
+      window.removeEventListener(resizeEvent + '.backgroundCheck');
+      window.removeEventListener('scroll.backgroundCheck');
+    }
+
     supported = null;
     canvas = null;
     context = null;
@@ -241,7 +246,7 @@
    */
   function kill(start) {
     var duration = new Date().getTime() - start;
-    
+
     log('Duration: ' + duration + 'ms');
 
     if (duration > get('maxDuration')) {
@@ -540,7 +545,7 @@
     for (var t = 0; t < targets.length; t++) {
       target = targets[t];
       target = get('changeParent') ? target.parentNode : target;
-      
+
       classList(target, get('classes').light, 'remove');
       classList(target, get('classes').dark, 'remove');
       classList(target, get('classes').complex, 'remove');
@@ -549,7 +554,7 @@
 
 
   /*
-   * Calculate average pixel brightness of a region 
+   * Calculate average pixel brightness of a region
    * and add 'light' or 'dark' accordingly
    */
   function calculatePixelBrightness(target) {
